97a306040b2ac53e3e3ae59cd0c155d8f92a7062
lachance123-franccois <lachanceawounang@gmail.com>
2025-11-08 15:30:27 +0100

    COMPILATION DE FICHIER CSV DE MEME CARACTERISQUES

---
+++ Compilation_de_dataset.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import glob
+
+path = r"C:\Users\AWOUNANG\Downloads\archive\nouveau dossier"
+ 
+#Récupère la liste de tous les fichiers CSV dans le dossier
+
+all_files = glob.glob(path + "/*.csv")
+
+dfs = []
+
+#chargement des fichiers contenus dans le dosssier.
+for f in all_files:
+    try:
+        df = pd.read_csv(f, sep=',', on_bad_lines='skip', low_memory=False)
+        dfs.append(df)
+        print(f"✅ Fichier chargé : {f} ({len(df)} lignes)")
+    except Exception as e:
+        print(f"❌ Erreur dans {f} : {e}")
+#Fusion de tous les fichiers en un seul DataFrame
+final_df = pd.concat(dfs, ignore_index=True)
+
+final_df.to_csv("nairobi_air_data_merged.csv", index=False)
+
+print(f"\n✅ Fusion terminée ! Nombre total de lignes : {len(final_df)}")
+
